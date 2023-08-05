e0d7c2b232d563d33c18a34f2f96eda965feb5cf
Hersh Patel <hgpatel715@gmail.com>
2023-08-05 00:31:01 -0400

    Adding patches

--- src/gossipNode.ts
+++ src/gossipNode.ts
@@ -1,6 +1,3 @@
-import { gossipsub, GossipSub } from "@chainsafe/libp2p-gossipsub";
-import { noise } from "@chainsafe/libp2p-noise";
-import { mplex } from "@libp2p/mplex";
 import { pubsubPeerDiscovery } from "@libp2p/pubsub-peer-discovery";
 import { tcp } from "@libp2p/tcp";
 import { createLibp2p, Libp2p } from "libp2p";
@@ -8,6 +5,34 @@ import { MultiaddrConnection } from "@libp2p/interface-connection";
 import { ConnectionGater } from "@libp2p/interface-connection-gater";
 import { PeerId } from "@libp2p/interface-peer-id";
 import { Multiaddr } from "@multiformats/multiaddr";
+import { gossipsub } from "@chainsafe/libp2p-gossipsub";
+import { mplex } from "@libp2p/mplex";
+import { noise } from "@chainsafe/libp2p-noise";
+import { Message as GossipSubMessage } from "@libp2p/interface-pubsub";
+import { msgId } from "@libp2p/pubsub/utils";
+
+/* This has been imported from the libp2p-gossipsub implementation as it's not public there */
+export const msgIdFnStrictSign = (message: GossipSubMessage): Uint8Array => {
+  if (message.type !== "signed") {
+    throw new Error("expected signed message type");
+  }
+  // Should never happen
+  if (message.sequenceNumber == null) throw Error("missing seqno field");
+
+  return msgId(message.from.toBytes(), message.sequenceNumber);
+};
+
+function text2Binary(string_: string) {
+  const a = new TextEncoder();
+  return a.encode(string_);
+}
+
+const MultiaddrLocalHost = "/ip4/127.0.0.1";
+const peerDiscoveryTopic = `_farcaster.0.peer_discovery`;
+
+function getMessageId(message: GossipSubMessage): Uint8Array {
+  return msgIdFnStrictSign(message);
+}
 
 /**
  * ConnectionFilter ensures that nodes only collect to peers in a specific allowlist.
@@ -140,18 +165,13 @@ export class ConnectionFilter implements ConnectionGater {
   }
 }
 
-const MultiaddrLocalHost = "/ip4/127.0.0.1";
-const peerDiscoveryTopic = `_farcaster.0.peer_discovery`;
-
 const listenMultiAddrStr = `${MultiaddrLocalHost}/tcp/${0}`;
 const createNode = async () => {
   const gossip = gossipsub({
     emitSelf: false,
     allowPublishToZeroPeers: true,
     globalSignaturePolicy: "StrictSign",
-    msgIdFn: (msg) => {
-      return msg.data;
-    },
+    msgIdFn: getMessageId,
     directPeers: [],
     canRelayMessage: true,
     scoreThresholds: {},
@@ -166,7 +186,10 @@ const createNode = async () => {
     transports: [tcp()],
     streamMuxers: [mplex()],
     connectionEncryption: [noise()],
-    pubsub: gossip,
+    services: {
+      pubsub: gossip,
+    },
+    //@ts-ignore
     peerDiscovery: [pubsubPeerDiscovery({ topics: [peerDiscoveryTopic] })],
   });
 
@@ -194,6 +217,20 @@ const main = async () => {
     await node.start();
     nodes.push(node);
   }
+
+  for (const node of nodes) {
+    //@ts-ignore
+    // node.services["pubsub"]!.addEventListener("gossipsub:message", (event) => {
+    //   console.log("gossipsub:message", {
+    //     identity: node.peerId.toString(),
+    //     event,
+    //   });
+    // });
+    // node?.addEventListener("peer:discovery", (event) => {
+    //   console.log("peer:discovery", event.detail.id);
+    // });
+  }
+
   try {
     const mainNode = nodes[0]!;
     for (const n of nodes.slice(1)) {
@@ -201,21 +238,69 @@ const main = async () => {
       await sleep(200);
       for (const multiAddr of mainNode.getMultiaddrs()) {
         const result = await n.dial(multiAddr);
-        assert(result.stat.status === "OPEN", "Connection not open");
+        assert(result.status === "open", "Connection not open");
       }
     }
 
     const primaryTopic = `f_network_${0}_primary`;
     // Subscribe each node to the test topic
-    nodes.forEach((n) => n.pubsub?.subscribe(primaryTopic));
+
+    for (const node of nodes) {
+      //@ts-ignore
+      await node.services["pubsub"]?.subscribe(primaryTopic);
+    }
+
     await sleep(PROPAGATION_DELAY);
 
-    nodes.map((n) =>
-      assert(n.pubsub.getPeers().length > 0, "Mesh did not form")
+    nodes.map((n) => console.log(n.getPeers()));
+    nodes.map((n) => {
+      //@ts-ignore
+      //console.log("Connection length", n.services["pubsub"]?.getPeers().length);
+      //@ts-ignore
+      assert(n.services["pubsub"]?.getPeers().length > 0, "Mesh did not form");
+    });
+    console.log("Mesh created");
+
+    const randomNode = nodes[NUM_NODES - 3]!;
+    //@ts-ignore
+    const publishResult = await randomNode.services["pubsub"]!.publish(
+      primaryTopic,
+      text2Binary("test")
     );
+
+    console.log(publishResult);
+    await sleep(3000);
+    let i = 1;
+    for (const vs of [...Array(10)]) {
+      console.log("Round", i);
+      console.log("\n");
+      await sleep(3000);
+      nodes.map((n) => {
+        console.log("\n");
+        console.log(
+          `Node ${n.peerId.toString()} has ${n.getPeers().length} peers`
+        );
+        console.log("\tThe peers are:\n");
+
+        n.getPeers().forEach((peer) => {
+          console.log(`\t\t ${peer.toString()}`);
+        });
+
+        assert(
+          //@ts-ignore
+          n.services["pubsub"]?.getPeers().length > 0,
+          "Mesh did not form"
+        );
+        console.log("\n");
+      });
+      console.log("\n");
+      i++;
+    }
   } catch (err) {
     console.error(err);
     nodes.forEach((node) => node.stop());
+    console.error("FAILED");
+    return;
   }
 
   nodes.forEach((node) => node.stop());
